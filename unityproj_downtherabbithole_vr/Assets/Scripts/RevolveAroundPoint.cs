49e8e5b442759d958aa9134c8f7d2a8f196bdc55
natalielh <nlehuenen@gmail.com>
2018-11-15 13:43:41 -0500

    Add RevolveAroundPoint.cs script

---
+++ unityproj_downtherabbithole_vr/Assets/Scripts/RevolveAroundPoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RevolveAroundPoint : MonoBehaviour {
+
+    [Range(-3.0f, 3.0f)]
+    public float speed;
+
+    public Vector3 point;
+
+    // Use this for initialization
+    void Start () {
+
+        point = new Vector3(0,0,0);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+
+	}
+}
