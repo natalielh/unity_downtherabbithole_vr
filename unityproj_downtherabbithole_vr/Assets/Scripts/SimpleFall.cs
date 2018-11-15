64346ee94c613016f34df73d587c2e3e150ef539
natalielh <nlehuenen@gmail.com>
2018-11-15 11:51:53 -0500

    Update new scene with faller script

---
+++ unityproj_downtherabbithole_vr/Assets/Scripts/SimpleFall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleFall : MonoBehaviour {
+
+    [Range(-3.0f, 3.0f)]
+    public float speed;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(speed * Vector3.up * Time.deltaTime, Space.World);
+
+    }
+}
