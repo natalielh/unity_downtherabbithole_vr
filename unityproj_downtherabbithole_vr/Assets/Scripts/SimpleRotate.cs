90f9e768edd258206b994dbaf95078d2c67e0167
natalielh <nlehuenen@gmail.com>
2018-11-15 17:39:28 -0500

    Apply test scripts to objects

---
+++ unityproj_downtherabbithole_vr/Assets/Scripts/SimpleRotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRotate : MonoBehaviour {
+
+    [Range(-20.0f, 20.0f)]
+    public float degrees_per_second;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Rotate the object around its local Y axis at speed degrees per second
+        transform.Rotate(degrees_per_second * Vector3.up * Time.deltaTime);
+
+    }
+}
