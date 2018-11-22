fa6a80d2789deecb825a03b23be22b4d31d536ef
natalielh <nlehuenen@gmail.com>
2018-11-21 21:48:01 -0500

    Setup primary object follow test scripts

---
+++ unityproj_downtherabbithole_vr/Assets/Scripts/PrimaryObjectFollowPlayer.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrimaryObjectFollowPlayer : MonoBehaviour {
+
+    public GameObject player_transform_input;
+    //public GameObject[] primary_objects;
+    public List<GameObject> primary_objects;
+
+    public int objects_met = 0;
+
+    public float seconds;
+
+    //private bool is_coroutine_executing = false;
+    //private readonly float detection_threshold = 0.1f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        for (int i = 0; i < primary_objects.Count; i++){
+
+            if(player_transform_input.transform.position.y <= primary_objects[i].transform.position.y)
+            {
+
+            }
+        }
+
+	}
+
+    //void CheckForObject(int array_index){
+    //    if (player_transform_input.transform.position.y <= primary_objects[array_index].transform.position.y)
+    //    {
+
+    //    }
+    //}
+
+    void ChangeParent(GameObject game_object, GameObject target_parent) {
+
+        game_object.transform.parent = target_parent.transform;
+
+    }
+
+    //IEnumerator ExecuteAfterTime(float time) {
+
+    //    if (is_coroutine_executing)
+    //        yield break;
+
+    //    is_coroutine_executing = true;
+
+    //    yield return new WaitForSeconds(time);
+
+    //    // Code to execute after the delay
+
+
+    //    is_coroutine_executing = false;
+    //}
+
+
+
+}
---
+++ unityproj_downtherabbithole_vr/Assets/Scripts/PrimaryObjectFollowPlayerNew.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrimaryObjectFollowPlayerNew : MonoBehaviour {
+
+    public GameObject player_transform_input;
+    //public GameObject new_parent_after_event;
+
+    // flag indicating whether or not the event has happened
+    private bool passed = false;
+
+    public readonly float seconds = 5.0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(!passed && player_transform_input.transform.position.y <= this.transform.position.y){
+        
+            this.transform.parent = player_transform_input.transform;
+
+            Invoke("ChangeParentToNull", seconds);
+
+            passed = true;
+        }
+
+	}
+
+    void ChangeParentToNull(){
+        this.transform.parent = null;
+    }
+}
