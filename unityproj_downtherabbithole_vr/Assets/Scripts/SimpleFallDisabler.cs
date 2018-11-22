ae9659b44b6a89bea0a8fbc2abc232452845211a
natalielh <nlehuenen@gmail.com>
2018-11-22 11:35:44 -0500

    Add SimpleFallDisabler.cs and enable fall disabling in scene

---
+++ unityproj_downtherabbithole_vr/Assets/Scripts/SimpleFallDisabler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleFallDisabler : MonoBehaviour {
+
+    public GameObject player_transform_input;
+
+    private bool passed = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!passed && player_transform_input.transform.position.y <= this.transform.position.y)
+        {
+
+            player_transform_input.GetComponent<SimpleFall>().enabled = false;
+
+            // set flag to true
+            passed = true;
+        }
+
+    }
+}
